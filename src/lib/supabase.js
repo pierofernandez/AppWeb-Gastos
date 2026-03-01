f3c19f7ce76d0a6d3949d28af0b2994d7b5aecc8
pierofernandez <pierofernandezz48@gmail.com>
2026-03-01 00:58:49 -0500

    solución de errores en producción urls

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl || 'http://localhost:54321', supabaseAnonKey || 'public-anon-key');
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
